690057cffb0216458d2744ed89d1e006e65f507a
szymon <brachman.szymon@gmail.com>
2017-07-20 10:40:31 +0200

    complete lab 4

---
+++ lab4/lab-4-noun/src/main/java/com/example/lab4noun/Lab4NounApplication.java
@@ -0,0 +1,14 @@
+package com.example.lab4noun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Lab4NounApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4NounApplication.class, args);
+	}
+}
---
+++ lab4/lab-4-verb/src/main/java/com/example/lab4verb/Lab4VerbApplication.java
@@ -0,0 +1,14 @@
+package com.example.lab4verb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Lab4VerbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4VerbApplication.class, args);
+	}
+}
