9e7b715bf87786ed4305ea9cb6b625db8882a789
szymon <brachman.szymon@gmail.com>
2017-07-21 11:39:48 +0200

    Complete lab 7

---
+++ lab7/sentence-server/src/main/java/demo/controller/SentenceController.java
@@ -0,0 +1,38 @@
+package demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import demo.service.SentenceService;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SentenceController {
+
+	@Autowired
+	SentenceService sentenceService;
+	
+	
+	/**
+	 * Display a small list of Sentences to the caller:
+	 */
+	@GetMapping("/sentence")
+	public String getSentences() {
+		long start = System.currentTimeMillis();
+		String output =
+				"<h3>Some Sentences</h3><br/>" +
+						sentenceService.buildSentence() + "<br/><br/>" +
+						sentenceService.buildSentence() + "<br/><br/>" +
+						sentenceService.buildSentence() + "<br/><br/>" +
+						sentenceService.buildSentence() + "<br/><br/>" +
+						sentenceService.buildSentence() + "<br/><br/>"
+				;
+		long end = System.currentTimeMillis();
+		return output + "Elapsed time (ms): " + (end - start);
+	}
+
+}
